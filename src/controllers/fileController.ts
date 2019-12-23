3a0bd122186a83d4c36777ab4d5812429064c642
pegasus.devops <jibl@poste-devops-jbl-16gbram.pegasus.io>
2019-12-23 07:02:34 +0100

    travaux pour la mise en oeuvre de l'upload download de fichiers via l'API

---
+++ src/controllers/fileController.ts
@@ -0,0 +1,23 @@
+import { Post, Request, Response, Route, SuccessResponse, Tags } from 'tsoa'
+import * as express from 'express'
+import * as multer from 'multer'
+import { StatusError } from '../models/StatusError'
+@Route('files')
+export class FilesController {
+  @Tags('File')
+  @SuccessResponse('204', 'successful')
+  @Response('400', 'invalid file supplied')
+  @Post('uploadFile')
+  public async uploadImageFile (@Request() request: express.Request): Promise {
+    await this.handleFile(request, 'imageFile', 'zip', 'tmp', 'img.zip')
+    // file will be in request.imageFile, it is a buffer
+  }
+  private async handleFile (request: express.Request,
+                            requestField: string,
+                            filterExt: string,
+                            storePath: string,
+                            fileName?: string): Promise {
+    const fileFilter = (req: Express.Request, file: Express.Multer.File,
+                        callback: (error: Error | null, acceptFile: boolean) => void) => {
+      // accept image only
+      if (!file.originalname.match(`^(.*\\.((${filterExt})$))
---
+++ src/voir.ts
@@ -0,0 +1,38 @@
+
+uploadImageFile )) { return callback(new Error(‘Only zip files are allowed!’), false) } callback(null, true) } // store at disk
+
+///
+///
+///  STUPID
+///
+///
+
+uploadImageFile () {
+   return callback(new Error(‘Only zip files are allowed!’), false)
+}
+callback(null, true) } // store at disk
+
+    const storage = multer.diskStorage({
+      destination: (req, file, callback) => {callback(null, `${storePath}/`) },
+      filename: (req, file, callback) => { callback(null, fileName ? fileName : file.originalname) } }
+    )
+    const upload = multer({ storage, fileFilter })
+    const multerSingle = upload.single(requestField)
+    try {
+      await new Promise(async (resolve, reject) => {
+        try {
+          multerSingle(request, undefined, async (error) => {
+            if (error) { reject(error) } resolve()
+            }
+          )
+        }
+        catch (error) {
+          reject(error)
+        }
+      })
+    }
+    catch (error) {
+      throw new StatusError(400, ‘invalid upload’, error.toString())
+    }
+  }
+}
