f9d9bc8d2f43a07bf39781a272a1e50c94324fab
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2019-12-23 08:59:11 +0100

    Update fileController.ts

--- src/controllers/fileController.ts
+++ src/controllers/fileController.ts
@@ -1,7 +1,7 @@
 import { Post, Request, Response, Route, SuccessResponse, Tags } from 'tsoa'
 import * as express from 'express'
 import * as multer from 'multer'
-import { StatusError } from '../models/StatusError'
+// import { StatusError } from '../models/StatusError'
 
 @Route('files')
 export class FilesController {
@@ -9,7 +9,7 @@ export class FilesController {
   @SuccessResponse('204', 'successful')
   @Response('400', 'invalid file supplied')
   @Post('uploadFile')
-  public async uploadImageFile (@Request() request: express.Request): Promise {
+  public async uploadImageFile (@Request() request: express.Request): Promise<void> {
     await this.handleFile(request, 'imageFile', 'zip', 'tmp', 'img.zip')
     // file will be in request.imageFile, it is a buffer
   }
@@ -17,16 +17,10 @@ export class FilesController {
                             requestField: string,
                             filterExt: string,
                             storePath: string,
-                            fileName?: string): Promise {
-      const fileFilter = (req: Express.Request, file: Express.Multer.File,
-                          callback: (error: Error | null, acceptFile: boolean) => void) => {
-        // accept image only
-        if (!file.originalname.match(`^(.*\\.((${filterExt})$))
-        {
-          throw new Error('Something bad happened : filename of the file you\'re trying to upload, doesn\'t make it look like it actually is an image.');
-        } else {
+                            fileName?: string): Promise <void> {
 
-        }
-      }
+          console.log("Le fichier est bien au format attendu");
+
+
   }
 }
